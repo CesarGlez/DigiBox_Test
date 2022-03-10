518d64e06cecae41d42cd499c4589ea7d38ecf54
CesarGlez <43555180+CesarGlez@users.noreply.github.com>
2022-03-10 14:11:14 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
-require('dotenv').config();
+require('dotenv').config(); //settings for enviroment variables
 
-const express = require('express')
-// const { databaseService } = require('./database')
-
+const express = require('express') //Dependecie to Node
 const app = express();
 
 app.use(express.json());
 app.use('/customer', require('./routes/routes'))
 
-app.listen(3000, function () {
+
+app.listen(process.env.PORT || 3000, function () { //Route to server. Usin process.env.PORT in case exist a port
    console.log("Server on port 3000")
 });
 
